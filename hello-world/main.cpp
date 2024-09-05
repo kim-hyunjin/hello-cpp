b826becb6c35603fe1c3241a00a6ffbee67c170f
kim-hyunjin <hyunjin1612@gmail.com>
2024-09-05 12:14:30 +0900

    std::cout, cin

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
-int main() {
+int main() 
+{
+
+    int favorite_number;
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Please enter your favorite number: ";
+    std::cin >> favorite_number;
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+
     return 0;
 }
